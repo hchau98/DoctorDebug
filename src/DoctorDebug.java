869a75b00fe632ba670485976bbe08d806409fec
hchau98 <chauhenry02@gmail.com>
2020-04-14 12:43:52 -0400

    First Commit

---
+++ src/DoctorDebug.java
@@ -0,0 +1,18 @@
+public class DoctorDebug {
+
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        Boolean hasInsurance = false;
+        int temp = 102;
+        double numVisits = 5.02;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temprature: "+temp);
+        System.out.println("Number of vists: "+numVisits );
+
+
+    }
+}
